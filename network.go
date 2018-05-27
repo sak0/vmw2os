2226e3a548df73654fc50310f7687328d18b4f1f
CuiHaozhi <cuihaozhi@chinacloud.com.cn>
2018-05-27 10:35:15 +0800

    enable collect portgroups
    Signed-off-by: CuiHaozhi <cuihaozhi@chinacloud.com.cn>

--- network.go
+++ network.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"text/tabwriter"
 	
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/view"
@@ -39,12 +41,22 @@ func GetHostNetwork(ctx context.Context, c *govmomi.Client){
 	defer v.Destroy(ctx)
 	
 	var hss []mo.HostSystem
-	err = v.Retrieve(ctx, []string{"HostSystem"}, []string{"network"}, &hss)
+	err = v.Retrieve(ctx, []string{"HostSystem"}, []string{"config.network.portgroup", "summary"}, &hss)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, hs := range hss {
-		fmt.Printf("%v\n", hs)
+		fmt.Printf("***%s***\n", hs.Summary.Config.Name)
+		tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+		fmt.Fprintf(tw, "Name:\tVswtich:\tVlanId:\t\n")
+		for _, pg := range hs.Config.Network.Portgroup {
+			fmt.Fprintf(tw, "%s\t", pg.Spec.Name)
+			fmt.Fprintf(tw, "%s\t", pg.Spec.VswitchName)
+			fmt.Fprintf(tw,"%d\t", pg.Spec.VlanId)
+			fmt.Fprintf(tw, "\n")
+		}
+		tw.Flush()
+		fmt.Println()
 	}
 }
